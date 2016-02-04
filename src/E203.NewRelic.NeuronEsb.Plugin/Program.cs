0f020ca66a27604699757edd137227642b09fea1
Brent Pabst <brent.pabst@dudesolutions.com>
2016-02-04 21:50:23 +0800

    Initial code commit

---
+++ src/E203.NewRelic.NeuronEsb.Plugin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NewRelic.Platform.Sdk;
+using NewRelic.Platform.Sdk.Utils;
+
+namespace E203.NewRelic.NeuronEsb.Plugin
+{
+    internal class Program
+    {
+        private static readonly Logger Logger = Logger.GetLogger("Plugin");
+
+        private static int Main(string[] args)
+        {
+            Logger.Info("Starting Plugin");
+            try
+            {
+                var runner = new Runner();
+                runner.Add(new EsbAgentFactory());
+                runner.SetupAndRun();
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Exception occurred, unable to continue.\n{0}",e.Message);
+                return -1;
+            }
+
+            Logger.Info("Exiting Plugin");
+            return 0;
+        }
+    }
+}
