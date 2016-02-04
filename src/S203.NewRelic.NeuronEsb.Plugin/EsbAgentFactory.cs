be66796fda0d6c373a783509c8188c816c2dade9
Brent Pabst <brent.pabst@dudesolutions.com>
2016-02-04 21:45:47 +0800

    Initial commit

---
+++ src/S203.NewRelic.NeuronEsb.Plugin/EsbAgentFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NewRelic.Platform.Sdk;
+using NewRelic.Platform.Sdk.Utils;
+
+namespace E203.NewRelic.NeuronEsb.Plugin
+{
+    public class EsbAgentFactory : AgentFactory
+    {
+        private static readonly Logger Logger = Logger.GetLogger("Plugin");
+
+        public override Agent CreateAgentWithConfiguration(IDictionary<string, object> properties)
+        {
+            Logger.Info("Initializing Agent...");
+
+            var name = (string)properties["name"];
+            var host = (string)properties["host"];
+            var port = int.Parse(properties["port"].ToString());
+            var instance = (string)properties["instance"];
+
+            return new EsbAgent(name, host, port, instance);
+        }
+    }
+}
