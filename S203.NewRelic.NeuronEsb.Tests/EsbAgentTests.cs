046fb2812c6ae197111f96338e879a56f67ce38a
Brent Pabst <brent.pabst@dudesolutions.com>
2016-02-06 12:59:49 -0500

    Initial Unit Tests

---
+++ S203.NewRelic.NeuronEsb.Tests/EsbAgentTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace S203.NewRelic.NeuronEsb.Tests
+{
+    [TestFixture]
+    class EsbAgentTests
+    {
+        [Test]
+        public void Ctor_Null_Argument_Name()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var agent = new EsbAgent(null, "", 0, "");
+            });
+        }
+
+        [Test]
+        public void Ctor_Null_Argument_Host()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var agent = new EsbAgent("", null, 0, "");
+            });
+        }
+
+        [Test]
+        public void Ctor_Null_Argument_Instance()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var agent = new EsbAgent("", "", 0, null);
+            });
+        }
+    }
+}
